d4a280e6cba7f3cb1aac37d00b5331ebc7ea0249
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2023-03-24 00:45:57 +0100

    did stuff

--- app/src/main/java/com/larrex/myapplication/ui/HomeScreen.kt
+++ app/src/main/java/com/larrex/myapplication/ui/HomeScreen.kt
@@ -1,18 +1,27 @@
 package com.larrex.myapplication.ui
 
+import android.os.Handler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -21,8 +30,10 @@ import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.larrex.myapplication.R
 import com.larrex.myapplication.Util
+import com.larrex.myapplication.ui.theme.green
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun HomeScreen() {
     val systemUiController = rememberSystemUiController()
@@ -30,7 +41,56 @@ fun HomeScreen() {
     var searchValue by remember {
         mutableStateOf(TextFieldValue(""))
     }
+    val keyController = LocalSoftwareKeyboardController.current
 
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    val handler = Handler()
+    var showProcess by remember { mutableStateOf(false) }
+
+    if (sheetState.isVisible) {
+        ModalBottomSheet(
+            onDismissRequest = { /*TODO*/ },
+            sheetState = sheetState,
+            containerColor = Color.White,modifier = Modifier.height(600.dp)
+        ) {
+
+            Row(horizontalArrangement = Arrangement.SpaceAround) {
+                CenterAlignedTopAppBar(
+                    navigationIcon = {
+//                    IconButton(onClick = {
+//                        scope.launch {
+//                            sheetState.hide()
+//                        }
+//                    }) {
+//                        Icon(Icons.Rounded.Close, contentDescription = "Cancel")
+//                    }
+                    },
+                    title = {
+//                    Text("Search Result")
+                        Text(
+                            text = "Search Result",
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = Util.quicksand,
+                            style = TextStyle.Default,
+
+                            )
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.White)
+                )
+
+            }
+            LazyColumn() {
+                items(30) {
+                    Text(text = "Hi my number is $it")
+                }
+            }
+        }
+
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -40,6 +100,10 @@ fun HomeScreen() {
         LazyColumn() {
             item {
 
+                if (showProcess) {
+                    LinearProgressIndicator(color = green, modifier = Modifier.fillMaxWidth())
+                }
+
                 Text(
                     text = Util.getGreeting(),
                     textAlign = TextAlign.Center,
@@ -48,11 +112,13 @@ fun HomeScreen() {
                     fontWeight = FontWeight.Bold,
                     fontFamily = Util.quicksand,
                     style = TextStyle.Default,
-                    modifier = Modifier.padding(start = 20.dp, top = 25.dp, bottom = 15.dp)
-
+                    modifier = Modifier.padding(start = 20.dp, top = 30.dp, bottom = 15.dp)
                 )
 
-                Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
 
 //
 //                    Icon(
@@ -91,12 +157,43 @@ fun HomeScreen() {
                             fontFamily = Util.quicksand,
                             fontWeight = FontWeight.Bold,
                             fontSize = 15.sp,
-                        )
+                        ), keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                        keyboardActions = KeyboardActions(onSearch = {
+                            keyController?.hide()
+                            showProcess=true
+                            handler.postDelayed({
+                                showProcess=false
+                                scope.launch {
+                                    sheetState.show()
+                                }
+                            }, 2000)
+                        })
                     )
                 }
+//                Button(
+//                    onClick = {
+//                        scope.launch {
+//                            sheetState.show()
+//                        }
+//                    },
+//                    modifier = Modifier
+//                        .padding(bottom = 70.dp, top = 30.dp, start = 30.dp, end = 30.dp)
+//                        .fillMaxWidth()
+//                        .height(60.dp),
+//                    colors = ButtonDefaults.buttonColors(green)
+//                ) {
+//
+//                    Text(
+//                        text = "see bottom sheet",
+//                        fontWeight = FontWeight.Bold,
+//                        fontFamily = Util.quicksand,
+//                        color = Color.White
+//                    )
+
             }
         }
-
     }
 
 }
+
+
--- app/src/main/java/com/larrex/myapplication/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/larrex/myapplication/ui/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.larrex.myapplication.ui.viewmodel
 
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import com.larrex.myapplication.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,6 +11,7 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private var repository: Repository
     ) : ViewModel() {
+
 
         fun doStuff(){
             repository.getIgboApiResponse()
